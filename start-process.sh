471a9d5f23a11e14937e7154e57b2f9550bacc21
roberthsx <roberthenrrique@gmail.com>
2024-02-28 15:56:55 -0300

    ajuste de process

--- start-process.sh
+++ start-process.sh
@@ -1,16 +1,15 @@
 #!/bin/bash 
-container_name=template-api
+container_name=template-api-v1
 
 #ECR Login
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin awsaccountid.dkr.ecr.us-east-1.am
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 590184025050.dkr.ecr.us-east-1.amazonaws.com
 
 #Pulling image from ECR
-docker pull 590184025050.dkr.ecr.sa-east-1.amazonaws.com/api-events-prod:latest
+docker pull 590184025050.dkr.ecr.us-east-1.amazonaws.com/template-api-prod:latest
 
 ##Changing image tag
-docker image tag 590184025050.dkr.ecr.sa-east-1.amazonaws.com/api-events-prod:latest $container_name:latest
-
+docker image tag 590184025050.dkr.ecr.us-east-1.amazonaws.com/template-api-prod:latest $container_name:latest
 #stop and remove the current container docker rm -f $container_name
 
 #Creating and starting a docker container using a new image
-docker run -d -p 80:80 --name $container_name $container_name:latest
+docker run -d -p 3000:80 --name $container_name $container_name:latest
