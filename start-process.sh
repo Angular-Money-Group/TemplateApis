fe874f829a286ab18a9363715e6d1f172eb0e068
roberthsx <roberthenrrique@gmail.com>
2024-03-14 13:00:49 -0300

    ajuste shell docker

--- start-process.sh
+++ start-process.sh
@@ -10,11 +10,7 @@ docker pull 590184025050.dkr.ecr.us-east-1.amazonaws.com/template-api-prod:lates
 ##Changing image tag
 docker image tag 590184025050.dkr.ecr.us-east-1.amazonaws.com/template-api-prod:latest $container_name:latest
 #stop and remove the current container docker rm -f $container_name
-if [ ! "$(docker ps -a -q -f name=template-api-v1)" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=template-api-v1)" ]; then
-        # cleanup
-        docker rm template-api-v1
-    fi
-fi
+docker stop template-api-v1
+docker rm template-api-v1
 #Creating and starting a docker container using a new image
 docker run -d -p 3000:80 --name $container_name $container_name:latest
